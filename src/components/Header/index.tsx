a69ab8d10ba1e4305e5d06252a3ab24cff13436f
ncb6206 <ncb6206@gmail.com>
2023-07-12 15:43:51 +0900

    Feat : Header와 Layout 추가

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { IOwnerRepo } from '../models/api';
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = (props: IOwnerRepo) => {
+  return (
+    <HeaderDiv>
+      {props.org} / {props.repo}
+    </HeaderDiv>
+  );
 };
+
+const HeaderDiv = styled.div`
+  padding: 20px 30px;
+  margin: 10px;
+  text-align: center;
+  box-shadow: 0 4px 4px -4px black;
+  border-radius: 5px;
+`;
 
 export default Header;
