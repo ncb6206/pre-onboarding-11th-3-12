4fb0d7239f7d9c00dac6b35763886064597b91ae
ncb6206 <ncb6206@gmail.com>
2023-07-13 03:46:10 +0900

    Refactor : Context API와 get API적용

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,30 +1,61 @@
 import styled from '@emotion/styled';
 import { IList } from '../models/api';
+import { Link, useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import OrgRepoContext from '../../contexts/OrgRepoContext';
 
 const ListItem = (props: IList) => {
+  const navigate = useNavigate();
+  const { setNumber } = useContext(OrgRepoContext);
+
+  const onClickIssue = () => {
+    setNumber(props.number);
+    return navigate('/issuedetail');
+  };
+
   return (
     <>
-      <IssueListDiv>
+      {props.index !== 0 && props.index % 4 === 0 && (
+        <Link to="https://www.wanted.co.kr/">
+          <Ad
+            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100"
+            alt="ad"
+          />
+        </Link>
+      )}
+      <IssueListDiv onClick={onClickIssue}>
         <IssueListLeft>
           <IssueListHead>
             {props.number} {props.title}
           </IssueListHead>
           <IssueListBody>
-            작성자: {props.writer}, 작성일: {props.date}
+            <span>작성자: {props.writer}</span>
+            <span>작성일: {props.date}</span>
           </IssueListBody>
         </IssueListLeft>
-        <IssueListComment>코멘트: {props.comments}</IssueListComment>
+        <IssueListComment>
+          <span>코멘트</span>
+          <span>{props.comments}</span>
+        </IssueListComment>
       </IssueListDiv>
-      <IssueListLine />
     </>
   );
 };
 
+const Ad = styled.img`
+  width: 100%;
+`;
+
 const IssueListDiv = styled.div`
+  width: 90%;
   display: flex;
   flex-direction: row;
-  margin: 15px;
+  justify-content: space-between;
+  padding: 10px;
+  margin: 10px;
   cursor: pointer;
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
 `;
 
 const IssueListLeft = styled.div`
@@ -39,17 +70,17 @@ const IssueListHead = styled.div`
 
 const IssueListBody = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  gap: 5px;
+  margin-top: 5px;
 `;
 
 const IssueListComment = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-`;
-
-const IssueListLine = styled.div`
-  border-bottom: 1px solid black;
+  align-items: center;
+  gap: 5px;
 `;
 
 export default ListItem;
--- src/pages/IssueDetail/index.tsx
+++ src/pages/IssueDetail/index.tsx
@@ -1,31 +1,54 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import styled from '@emotion/styled';
-import ListItem from '../../components/ListItem';
+import { useNavigate } from 'react-router-dom';
+import OrgRepoContext from '../../contexts/OrgRepoContext';
+import { getIssue } from '../../services/Issue';
+import Loading from '../../components/Loading';
+import dating from '../../components/Common/Utils/dating';
 
 const IssueDetail = () => {
+  const navigate = useNavigate();
+  const [issue, setIssue] = useState<any>([]); // 수정필요
+  const { org, repo, number } = useContext(OrgRepoContext);
+
+  const getItem = async () => {
+    const response = await getIssue({ org, repo, number });
+    // console.log(response, response.data);
+    if (response.status === 200) {
+      setIssue(response.data);
+    }
+  };
+
+  useEffect(() => {
+    if (!(repo && org && number)) return navigate('/');
+    getItem();
+    // console.log(issue);
+  }, []);
+
   return (
     <Wrap>
-      {/* <Header /> */}
+      <Header />
       <IssueDetailDiv>
-        <IssueDetailHead>
-          <div>avatar</div>
-          <ListItem
-            key={IssueDummy.id}
-            id={IssueDummy.id}
-            number={IssueDummy.number}
-            title={IssueDummy.title}
-            writer={IssueDummy.user.login}
-            date={IssueDummy.updated_at}
-            comments={IssueDummy.comments}
-          />
-        </IssueDetailHead>
-        <IssueDetailBody>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores at
-          consequuntur cumque molestias placeat error cum laborum enim,
-          consequatur sequi impedit iusto nihil id repellat numquam non voluptas
-          ullam dicta?
-        </IssueDetailBody>
+        {issue.length !== 0 && (
+          <>
+            <IssueDetailHead>
+              <Avatar src={issue.user.avatar_url} alt="avatar" />
+              <IssueDetailLeft>
+                <IssueDetailTitle>
+                  {issue.number} {issue.title}
+                </IssueDetailTitle>
+                <IssueDetailWrite>
+                  <span>작성자: {issue.user.login}</span>
+                  <span>작성일: {dating(issue.updated_at)}</span>
+                </IssueDetailWrite>
+              </IssueDetailLeft>
+              <IssueDetailComment>코멘트: {issue.comments}</IssueDetailComment>
+            </IssueDetailHead>
+            <IssueDetailBody>{issue.body}</IssueDetailBody>
+          </>
+        )}
+        {issue.length === 0 && <Loading />}
       </IssueDetailDiv>
     </Wrap>
   );
@@ -35,6 +58,10 @@ export default IssueDetail;
 
 const Wrap = styled.main`
   width: 80%;
+  height: 100vh;
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  padding: 2rem;
 `;
 
 const IssueDetailDiv = styled.div`
@@ -48,20 +75,41 @@ const IssueDetailHead = styled.div`
   flex-direction: row;
   align-items: center;
   gap: 1rem;
+  margin: 2rem 0;
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+`;
+
+const Avatar = styled.img`
+  width: 15%;
+  height: 15%;
 `;
 
 const IssueDetailBody = styled.div`
   display: flex;
   flex-direction: column;
+  line-height: 1.5;
 `;
 
-const IssueDummy = {
-  id: 1232114,
-  number: '#111',
-  title: 'issue title',
-  user: {
-    login: 'name',
-  },
-  updated_at: '2019년 12월 31일',
-  comments: '67',
-};
+const IssueDetailLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const IssueDetailTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const IssueDetailWrite = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  margin-top: 5px;
+`;
+
+const IssueDetailComment = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
