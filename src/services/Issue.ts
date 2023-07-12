93033878330adc97aecee26cffc20887aa26b123
ncb6206 <ncb6206@gmail.com>
2023-07-13 03:41:54 +0900

    Feat : 특정 Issue 데이터 받아오는 axios 추가

--- src/services/Issue.ts
+++ src/services/Issue.ts
@@ -1,11 +1,21 @@
-import { IOwnerRepo } from '../components/models/api';
+import { IGetIssues, IGetIssue } from '../components/models/api';
 import instance from './config';
 
-export const getIssue = async (props: IOwnerRepo) => {
+export const getIssues = async ({ org, repo, page }: IGetIssues) => {
   try {
     const res = await instance.get(
-      `/${props.org}/${props.repo}/issues?state=open&sort=comments`,
+      `/${org}/${repo}/issues?state=open&sort=comments&page=${page}&per_page=10`,
     );
+    return res;
+  } catch (error: any) {
+    console.error(error);
+    return error;
+  }
+};
+
+export const getIssue = async ({ org, repo, number }: IGetIssue) => {
+  try {
+    const res = await instance.get(`/${org}/${repo}/issues/${number}`);
     return res;
   } catch (error: any) {
     console.error(error);
