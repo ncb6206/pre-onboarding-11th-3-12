b82ad9d6351cc48c1a7d61b47c4fc5940696e12e
ncb6206 <ncb6206@gmail.com>
2023-07-13 03:40:16 +0900

    Feat : 날짜 변환 기능 추가

---
+++ src/components/Common/Utils/dating.ts
@@ -0,0 +1,13 @@
+const dating = (time: string) => {
+  const date = new Date(time);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분 ${seconds}초`;
+};
+
+export default dating;
