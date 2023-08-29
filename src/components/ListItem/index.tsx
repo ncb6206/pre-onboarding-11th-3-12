a145332ba0a37f4f1708a0328e12fa3b26a309ed
ncb6206 <ncb6206@gmail.com>
2023-08-30 03:10:39 +0900

    Refactor : 광고 배너 변경

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -16,9 +16,9 @@ const ListItem = (props: IList) => {
   return (
     <>
       {props.index !== 0 && props.index % 4 === 0 && (
-        <Link to="https://www.wanted.co.kr/">
+        <Link to="https://github.com/ncb6206/">
           <Ad
-            src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100"
+            src="https://camo.githubusercontent.com/610a398f79b84273e9306ec9f58eeebb25aa7b8bab5563ceb3448646a5be4ebd/68747470733a2f2f63617073756c652d72656e6465722e76657263656c2e6170702f6170693f747970653d7472616e73706172656e7426636f6c6f723d6175746f26637573746f6d436f6c6f724c6973743d3236266865696768743d3135302673656374696f6e3d68656164657226746578743d497427732532304e6125323047697468756226666f6e7453697a653d393026616e696d6174696f6e3d66616465496e"
             alt="ad"
           />
         </Link>
@@ -26,7 +26,7 @@ const ListItem = (props: IList) => {
       <IssueListDiv onClick={onClickIssue}>
         <IssueListLeft>
           <IssueListHead>
-            {props.number} {props.title}
+            #{props.number} {props.title}
           </IssueListHead>
           <IssueListBody>
             <span>작성자: {props.writer}</span>
@@ -44,6 +44,8 @@ const ListItem = (props: IList) => {
 
 const Ad = styled.img`
   width: 100%;
+  height: 5vh;
+  margin-top: 0.5rem;
 `;
 
 const IssueListDiv = styled.div`
@@ -56,11 +58,13 @@ const IssueListDiv = styled.div`
   cursor: pointer;
   box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.2);
   border-radius: 5px;
+  background-color: #f8f6ee;
 `;
 
 const IssueListLeft = styled.div`
   display: flex;
   flex-direction: column;
+  width: 90%;
 `;
 
 const IssueListHead = styled.div`
