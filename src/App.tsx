f7bf2cc10262967eb181cc154d812b3dd4d617aa
ncb6206 <ncb6206@gmail.com>
2023-09-01 02:50:56 +0900

    Refactor : tanstack query 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './routes';
 import OrgRepoProvider from './contexts/OrgRepoProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <OrgRepoProvider>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
-    </OrgRepoProvider>
+    <QueryClientProvider client={queryClient}>
+      <OrgRepoProvider>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </OrgRepoProvider>
+    </QueryClientProvider>
   );
 }
 
